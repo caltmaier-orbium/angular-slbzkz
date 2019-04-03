6f583bffede50168fde7eb48597f9b4d1cb54a15
caltmaier-orbium <cedric.altmaier@orbium.com>
2019-04-03 13:08:55 +0200

    barchart component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,23 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
+import { ChartsModule } from 'ng2-charts';
+import { Routes, RouterModule } from '@angular/router';
+
+import { MyBarChartComponent } from './my-bar-chart/my-bar-chart.component';
+
+const routes: Routes = [
+  {path: 'bar-chart', component: MyBarChartComponent},
+ /* {path: 'doughnut-chart', component: MyDoughnutChartComponent},
+  {path: 'radar-chart', component: MyRadarChartComponent},
+  {path: 'pie-chart', component: MyPieChartComponent}, */
+  {path: '**', component: MyBarChartComponent }
+];
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule,RouterModule.forRoot(routes),
+    ChartsModule ],
+  declarations: [ AppComponent, HelloComponent, MyBarChartComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
