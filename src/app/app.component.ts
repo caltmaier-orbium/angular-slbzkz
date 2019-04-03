ea3587dc8b58f8a9998bafbf466e26e0cfe078c5
caltmaier-orbium <cedric.altmaier@orbium.com>
2019-04-03 13:01:50 +0200

    charts basic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  name = 'Angular';
+export class AppComponent implements OnInit {
+
+  chart: Chart;
+  name = 'Angular 5 chartjs';
+
+  ngOnInit() {
+    this.chart = new Chart('canvas', {
+      type: 'doughnut',
+      data: {
+        labels: ['Solicitado', 'Entregado', 'Faltante'],
+        datasets: [
+          {
+            label: 'test',
+            data: [
+              100, 200, 300
+            ],
+            backgroundColor: ['#0074D9', '#2ECC40', '#FF4136']
+          }
+        ]
+      },
+      options: {
+        title: {
+          display: false,
+          text: 'Color test'
+        },
+        legend: {
+          position: 'left',
+          display: false,
+          fullWidth: false,
+          labels: {
+            fontSize: 11
+          }
+        },
+        scales: {
+          xAxes: [{
+            display: false
+          }],
+          yAxes: [{
+            display: false
+          }]
+        }
+      }
+    });
+  }
 }
