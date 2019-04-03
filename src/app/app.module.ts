3cb1d3964d1954efaa4524faa1a794bbe6c4c7a5
caltmaier-orbium <cedric.altmaier@orbium.com>
2019-04-03 13:14:23 +0200

    doughnutchart w/ options

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,12 @@ import { ChartsModule } from 'ng2-charts';
 import { Routes, RouterModule } from '@angular/router';
 
 import { MyBarChartComponent } from './my-bar-chart/my-bar-chart.component';
+import { MyDoughnutChartComponent } from './my-doughnut-chart/my-doughnut-chart.component';
 
 const routes: Routes = [
   {path: 'bar-chart', component: MyBarChartComponent},
- /* {path: 'doughnut-chart', component: MyDoughnutChartComponent},
-  {path: 'radar-chart', component: MyRadarChartComponent},
+  {path: 'doughnut-chart', component: MyDoughnutChartComponent},
+ /* {path: 'radar-chart', component: MyRadarChartComponent},
   {path: 'pie-chart', component: MyPieChartComponent}, */
   {path: '**', component: MyBarChartComponent }
 ];
@@ -21,7 +22,7 @@ const routes: Routes = [
 @NgModule({
   imports:      [ BrowserModule, FormsModule,RouterModule.forRoot(routes),
     ChartsModule ],
-  declarations: [ AppComponent, HelloComponent, MyBarChartComponent ],
+  declarations: [ AppComponent, HelloComponent, MyBarChartComponent, MyDoughnutChartComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
