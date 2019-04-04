b57531bce85ef891edfb37e0981c9f0e6b01c246
caltmaier-orbium <cedric.altmaier@orbium.com>
2019-04-04 15:52:19 +0200

    DataService for MockData

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -3,7 +3,23 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class DataService {
 
-  constructor() { }
+  CHART_DATA;
+  CHART_DATA1;
+  CHART_DATA2;
+
+  constructor() {
+  }
+
+  public getMockDataElement(){
+    this.CHART_DATA = this.getMockDataBar();
+    this.CHART_DATA1 = this.getMockDataDoughnut();
+    this.CHART_DATA2 = this.getMockDataPie();
+
+    return [
+        {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H', chartData: this.CHART_DATA},
+        {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He', chartData: this.CHART_DATA1},
+        {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li', chartData: this.CHART_DATA2} ];
+  }
 
   getMockDataBar(){
     return { 
@@ -17,19 +33,19 @@ export class DataService {
           chartOptions: { scaleShowVerticalLines: false,
                       responsive: true }
       };
-    
-  }
+  };
 
-  getMockDataDoughnut(){
+ getMockDataDoughnut(){
     return { 
             chartType: 'doughnut', 
             chartLabels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'], 
             chartLegendEnabled: false, 
-            chartData:  
-                          {data: [65, 59, 80, 81, 56, 55, 40], label: 'Test1'},
+            chartData:  [
+                          {data: [65, 59, 80, 81, 56, 55, 40]}
+                        ],
             chartOptions: { scaleShowVerticalLines: false,
                             responsive: true }
-    } 
+}
   }
 
   getMockDataPie(){
@@ -44,9 +60,4 @@ export class DataService {
                               responsive: true }
     }
   }
-
-  getMockDataTable(){
-    
-  }
-
 }
--- src/app/dyn-table-chart/dyn-table-chart.component.ts
+++ src/app/dyn-table-chart/dyn-table-chart.component.ts
@@ -7,48 +7,6 @@ import {MasterDataChart} from '../model/model.module';
 import {PeriodicElementChartData} from '../model/model.module';
 import {DataService} from '../data.service'
 
-const CHART_DATA: MasterDataChart = { 
-  chartType: 'bar', 
-  chartLabels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'], 
-  chartLegendEnabled: false, 
-  chartData:  [
-                {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-                {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
-              ],
-  chartOptions: { scaleShowVerticalLines: false,
-                  responsive: true }
-};
-
-const CHART_DATA1: MasterDataChart = { 
-  chartType: 'doughnut', 
-  chartLabels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'], 
-  chartLegendEnabled: false, 
-  chartData:  [
-                {data: [65, 59, 80, 81, 56, 55, 40], label: 'Test1'},
-                {data: [28, 48, 40, 19, 86, 27, 90], label: 'Test2'}
-              ],
-  chartOptions: { scaleShowVerticalLines: false,
-                  responsive: true }
-};
-
-const CHART_DATA2: MasterDataChart = { 
-  chartType: 'pie', 
-  chartLabels: ['2006', '2007', '2008', '2009'], 
-  chartLegendEnabled: false, 
-  chartData:  [
-                {data: [65, 59, 80, 81]}
-              ],
-  chartOptions: { scaleShowVerticalLines: false,
-                  responsive: true }
-};
-
-const ELEMENT_DATA: PeriodicElementChartData[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H', chartData: CHART_DATA},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He', chartData: CHART_DATA1},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li', chartData: CHART_DATA2}
-];
-
-
 @Component({
   selector: 'app-dyn-table-chart',
   templateUrl: './dyn-table-chart.component.html',
@@ -56,14 +14,19 @@ const ELEMENT_DATA: PeriodicElementChartData[] = [
 })
 export class DynTableChartComponent implements OnInit {
 
-  public dataForChart = CHART_DATA;
+  public dataService: DataService = new DataService;
+
+  ELEMENT_DATA;
 
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'chart'];
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
+
+  dataSource; 
 
   @ViewChild(MatSort, {static: true}) sort: MatSort;
 
-  ngOnInit() {
+  ngOnInit() {    
+    this.ELEMENT_DATA = this.dataService.getMockDataElement();
+    this.dataSource = new MatTableDataSource(this.ELEMENT_DATA);
     this.dataSource.sort = this.sort;
   }
 }
