40fd5f495f9e584ae03bdbf8340872cc2691cbd0
caltmaier-orbium <cedric.altmaier@orbium.com>
2019-04-08 10:57:28 +0200

    expandable item

--- src/app/tree-table/tree-table.component.ts
+++ src/app/tree-table/tree-table.component.ts
@@ -2,31 +2,54 @@ import { Component, OnInit } from '@angular/core';
 import { Node, Options } from 'ng-material-treetable';
 import {Report, Task} from '../model/model.module';
 import {DataService} from '../data.service';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-tree-table',
   templateUrl: './tree-table.component.html',
-  styleUrls: ['./tree-table.component.css']
+  styleUrls: ['./tree-table.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0', display: 'none'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ]
 })
-export class TreeTableComponent implements OnInit {
+export class TreeTableComponent {
 
 public dataService: DataService = new DataService;
 
-  treeOptions: Options<Report> = {
-    capitalisedHeader: true,
-    customColumnOrder: [
-      'owner', 'name', 'backup', 'protected'
-    ]
-  };
+dataSource = ELEMENT_DATA;
+columnsToDisplay = ['name', 'weight', 'symbol', 'position'];
+  expandedElement: PeriodicElement | null;
 
-  singleRootTree: Node<Report> = this.dataService.getMockDataTreeReport();
-
-  arrayOfNodesTree: Node<Task>[] = this.dataService.getMockDataTreeTasks();
-
-  logNode(node: Node<Report>) {
-    console.log(node);
-  }
-
-  ngOnInit() {
-  }
 }
+
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+  children: PeriodicElement[] | null;
+}
+
+const child: PeriodicElement = 
+{
+    position: 2,
+    name: 'Hydrogen',
+    weight: 1.0079,
+    symbol: 'H',
+    children: null
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {
+    position: 1,
+    name: 'Hydrogen',
+    weight: 1.0079,
+    symbol: 'H',
+    children: child
+  }
+]
