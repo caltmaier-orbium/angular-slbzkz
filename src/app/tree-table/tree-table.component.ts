52e6fec4147c654b48eb485db441cc32cf653a82
caltmaier-orbium <cedric.altmaier@orbium.com>
2019-04-08 13:07:29 +0200

    implemented material tree-table

--- src/app/tree-table/tree-table.component.ts
+++ src/app/tree-table/tree-table.component.ts
@@ -7,49 +7,20 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
 @Component({
   selector: 'app-tree-table',
   templateUrl: './tree-table.component.html',
-  styleUrls: ['./tree-table.component.css'],
-  animations: [
-    trigger('detailExpand', [
-      state('collapsed', style({height: '0px', minHeight: '0', display: 'none'})),
-      state('expanded', style({height: '*'})),
-      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
-    ]),
-  ]
+  styleUrls: ['./tree-table.component.css']
 })
 export class TreeTableComponent {
 
+treeOptions: Options<Report> = {
+    capitalisedHeader: true,
+    customColumnOrder: [
+      'owner', 'name', 'backup', 'protected'
+    ]
+  };
 public dataService: DataService = new DataService;
 
-dataSource = ELEMENT_DATA;
-columnsToDisplay = ['name', 'weight', 'symbol', 'position'];
-  expandedElement: PeriodicElement | null;
+singleRootTree: Node<Report> = this.dataService.getMockDataTreeReport();
+arrayOfNodesTree: Node<Task>[] = this.dataService.getMockDataTreeTasks();
+
 
 }
-
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-  children: PeriodicElement[] | null;
-}
-
-const child: PeriodicElement = 
-{
-    position: 2,
-    name: 'Hydrogen',
-    weight: 1.0079,
-    symbol: 'H',
-    children: null
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {
-    position: 1,
-    name: 'Hydrogen',
-    weight: 1.0079,
-    symbol: 'H',
-    children: child
-  }
-]
