01afb01367ba9e512fb447b8cba9d7654d733382
caltmaier-orbium <cedric.altmaier@orbium.com>
2019-04-11 13:45:37 +0200

    created interface

--- src/app/exp-tree/exp-tree.component.ts
+++ src/app/exp-tree/exp-tree.component.ts
@@ -33,6 +33,11 @@ export interface Element {
   symbol: string;
 }
 
+export interface ElementData {
+  parent: Element;
+  children: Element | Element[] | null;
+}
+
 const ELEMENT_DATA: Element[] = [
   {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
   {position: 3, name: 'Helium', weight: 4.0026, symbol: 'He'},
@@ -41,4 +46,12 @@ const ELEMENT_DATA: Element[] = [
   {position: 6, name: 'Boron', weight: 10.811, symbol: 'B'},
   {position: 7, name: 'Carbon', weight: 12.0107, symbol: 'C'},
   {position: 8, name: 'Nitrogen', weight: 14.0067, symbol: 'N'}
+  ];
+
+  const ELEMENT_DATA_COLLECTION: ElementData[] = [
+  {parent: ELEMENT_DATA[0], children: null},
+  {parent: ELEMENT_DATA[1], children: ELEMENT_DATA[0]},
+  {parent: ELEMENT_DATA[1], children: [ELEMENT_DATA[0],ELEMENT_DATA[4]]},
+  {parent: ELEMENT_DATA[3], children: null},
+  {parent: ELEMENT_DATA[4], children: null},
   ];
